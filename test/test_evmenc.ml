f84e4a34d435d44a860f0bb3ad6a965d3af00e08
Julian Nagele <mail@jnagele.net>
2018-09-28 17:23:30 +0100

    Rename function

--- test/test_evmenc.ml
+++ test/test_evmenc.ml
@@ -16,16 +16,16 @@ let solve_model_exn cs =
   | Z3.Solver.UNSATISFIABLE -> failwith "UNSAT"
   | Z3.Solver.UNKNOWN -> failwith (Z3.Solver.get_reason_unknown slvr)
 
-let eval_state_field m i ?(n=[]) f =
+let eval_func_decl_at_i m i ?(n = []) f =
   match Z3.Model.eval m (f <@@> ([num i] @ n)) true with
   | Some e -> e
   | None -> failwith ("could not eval " ^ Z3.FuncDecl.to_string f)
 
-let eval_stack st m i n = eval_state_field m i ~n:[bvnum n sas] st.stack
+let eval_stack st m i n = eval_func_decl_at_i m i ~n:[bvnum n sas] st.stack
 
-let eval_exc_halt st m i = eval_state_field m i st.exc_halt
+let eval_exc_halt st m i = eval_func_decl_at_i m i st.exc_halt
 
-let eval_gas st m i = eval_state_field m i st.used_gas
+let eval_gas st m i = eval_func_decl_at_i m i st.used_gas
 
 let eval_const m k =
   match Z3.Model.eval m k true with
@@ -259,7 +259,7 @@ let suite =
           ~cmp:[%eq: Z3.Expr.t]
           ~printer:Z3.Expr.to_string
           (num (opcode (PUSH 1)))
-          (eval_state_field m 0 fis)
+          (eval_func_decl_at_i m 0 fis)
       );
 
     "search for 1 instruction program (k)">::(fun _ ->
