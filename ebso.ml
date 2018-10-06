ceccca36efa66aafb81ae86244e62ead480be574
Maria A Schett <mail@maria-a-schett.net>
2018-10-06 23:26:40 +0100

    Clean up main

--- ebso.ml
+++ ebso.ml
@@ -7,28 +7,6 @@ let p0 = [
   ADD;
 ]
 
-let p0_optzd = [
-  PUSH (Val 2);
-]
-
-(* Goal 1: super_optimze p0 == p0_optzd *)
-
-let zero = [
-  PUSH (Val 42);
-  PUSH (Val 21);
-  PUSH (Val 0);
-  SUB;
-  ADD;
-]
-
-let zero_optzd = [
-  PUSH (Val 21);
-  PUSH (Val 42);
-  SUB;
-]
-
-(* Goal 2: super_optimze zero == zero_optzd *)
-
 let () =
   let open Command.Let_syntax in
   Command.basic ~summary:"ebso: An EVM Bytecode Super Optimizer"
@@ -37,9 +15,7 @@ let () =
       in
       fun () ->
         match file with
-        | None -> print_string @@ super_optimize zero [PUSH Tmpl; ADD; SUB]
+        | None -> print_string @@ super_optimize p0 [PUSH Tmpl; ADD; SUB]
         | Some _ -> print_string @@ super_optimize p0 []
     ]
   |> Command.run ~version:"0.1"
-
-  (* let p' = super_optimize zero in *)
