951bc9edc59749be2c3e21b5d963c616c8a5d212
Julian Nagele <mail@jnagele.net>
2018-10-13 15:01:20 +0100

    Split tests

--- test/test_evmenc.ml
+++ test/test_evmenc.ml
@@ -468,47 +468,6 @@ let suite =
           (List.init sk_size ~f:(eval_stack stt m 0))
       );
 
-    (* super optimize *)
-
-    "super optimize PUSH PUSH ADD to PUSH" >::(fun _ ->
-        let p = [PUSH (Val 1); PUSH (Val 1); ADD] in
-        let sis = `User [PUSH (Val 2); PUSH (Val 1); ADD; SUB] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [PUSH (Val 2)] (dec_super_opt ea m)
-      );
-
-    "super optimize PUSH and POP" >::(fun _ ->
-        let p = [PUSH (Val 1); POP;] in
-        let sis = `User [PUSH Tmpl; POP;] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [] (dec_super_opt ea m)
-      );
-
-    "super optimize x * 0 to POP; PUSH 0" >::(fun _ ->
-        let p = [PUSH (Val 0); MUL] in
-        let sis = `User [PUSH Tmpl; POP; MUL; ADD] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [POP; PUSH (Val 0)] (dec_super_opt ea m)
-      );
-
-    "super optimize x * 1 to x" >::(fun _ ->
-        let p = [PUSH (Val 1); MUL] in
-        let sis = `User [PUSH Tmpl; POP; MUL; ADD] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [] (dec_super_opt ea m)
-      );
 
     (* template argument for PUSH *)
 
@@ -581,25 +540,6 @@ let suite =
           [1; 1] [eval_a ea m 0; eval_a ea m 1]
       );
 
-    "super optimize PUSH PUSH ADD to PUSH with template" >::(fun _ ->
-        let open Z3Ops in
-        let p = [PUSH (Val 1); PUSH (Val 1); ADD] in
-        let ks = List.length p in
-        let sis = `User [PUSH Tmpl; ADD; SUB] in
-        let ea = mk_enc_consts p sis in
-        let sts = mk_state ea "_s" in
-        let stt = mk_state ea "_t" in
-        let c =
-          enc_program ea sts &&
-          enc_search_space ea stt &&
-          enc_equivalence ea sts stt &&
-          sts.used_gas @@ [num ks] > stt.used_gas @@ [ea.kt]
-        in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [PUSH (Val 2)] (dec_super_opt ea m)
-      );
-
     (* stack_depth *)
 
     "No negative stack depth, sufficient arguments" >::(fun _ ->
@@ -638,38 +578,6 @@ let suite =
         assert_bool "not all instructions present"
           (List.for_all [ADD; MUL; PUSH Tmpl; POP; SUB]
              ~f:(fun i -> List.mem all_of_instr i ~equal:[%eq: instr]))
-      );
-
-    (* enc_super_opt with init stack elements *)
-
-    "super optimize x + 0 with one init stack element" >::(fun _ ->
-        let p = [PUSH (Val 0); ADD] in
-        let sis = `User [PUSH Tmpl; ADD] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [] (dec_super_opt ea m)
-      );
-
-    "super optimize with two init stack elements" >: test_case ~length:Long (fun _ ->
-        let p = [ADD; PUSH (Val 0); ADD] in
-        let sis = `User [ADD] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [ADD] (dec_super_opt ea m)
-      );
-
-    "super optimize 3 + (0 - x) to (3 - x) " >::(fun _ ->
-        let p = [PUSH (Val 0); SUB; PUSH (Val 3); ADD;] in
-        let sis = `User [PUSH Tmpl; ADD; SUB;] in
-        let ea = mk_enc_consts p sis in
-        let c = enc_super_opt ea in
-        let m = solve_model_exn [c] in
-        assert_equal ~cmp:[%eq: progr] ~printer:[%show: progr]
-          [PUSH (Val 3); SUB] (dec_super_opt ea m)
       );
 
     (* SWAP *)
