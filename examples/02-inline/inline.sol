0834209641ac0837581ef8f0f8a9a9857ff41b32
Julian Nagele <mail@jnagele.net>
2018-10-28 16:56:09 +0000

    Fix example contract

--- examples/02-inline/inline.sol
+++ examples/02-inline/inline.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.4.0;
 
 contract Inline {
 
-  function c public pure returns (uint8) {
+  function c () public pure returns (uint8) {
     return 42;
   }
 
   function f (uint8 x) public returns (uint8) {
-    return x + c;
+    return x + c();
   }
 }
