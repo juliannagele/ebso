9549429470490c6669401ea3b2d4ea5f9766ea02
Julian Nagele <mail@jnagele.net>
2018-11-01 12:40:57 +0000

    Increase default stack size to avoid weird SWAP16 wrap around

--- lib/evmenc.ml
+++ lib/evmenc.ml
@@ -6,7 +6,7 @@ open Program
 (* stack address size; design decision/quick fix: the slot 2^sas - 1 is reserved
    for exception handling, otherwise the stack counter wraps around
    --> max stack size 2^sas - 1 *)
-let sas = ref 5
+let sas = ref 6
 (* stack element size *)
 let ses = ref 3
 let sasort = ref (bv_sort !sas)
