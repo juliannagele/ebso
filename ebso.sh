1142995f78b0d25d26de4656e39166773c70ce12
Julian Nagele <mail@jnagele.net>
2018-11-06 15:53:41 +0000

    Make calling ebso.sh from different directories possible

--- ebso.sh
+++ ebso.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 Z3DIR=$(ocamlfind query z3)
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Z3DIR _build/default/ebso.exe "$@"
+EBSODIR=$(dirname "$(realpath -s "$0")")
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Z3DIR "$EBSODIR"/_build/default/ebso.exe "$@"
