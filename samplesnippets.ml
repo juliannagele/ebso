7b39f976dff1150f950969fa28061a25a62d2672
Julian Nagele <mail@jnagele.net>
2018-11-14 12:09:20 +0000

    Also count number of equal snippets when sampling

--- samplesnippets.ml
+++ samplesnippets.ml
@@ -1,22 +1,34 @@
 open Core
 open Instruction
 
-let abstract_pusharg wsz sp =
-  Sedlexing.Latin1.from_string sp |> Parser.parse
-  |> Program.val_to_const wsz
-  |> List.mapi ~f:(fun j i ->
-      match i with PUSH (Const _) -> PUSH (Const (Int.to_string j)) | _ -> i)
+module Snippet_mod = struct
+  module T = struct
+    type t = string [@@deriving sexp]
+    let wsz = ref 0
 
-let equal_mod_pusharg wsz sp1 sp2 =
-  let p1 = abstract_pusharg wsz sp1
-  and p2 = abstract_pusharg wsz sp2 in
-  Program.equal p1 p2
+    let set_wsz s = wsz := s
 
-let compare_prog wsz sp1 sp2 =
-  if equal_mod_pusharg wsz sp1 sp2 then 0
-  else
-    let lc = Int.compare (String.length sp1) (String.length sp2) in
-    if lc <> 0 then lc else String.compare sp1 sp2
+    let abstract_pusharg sp =
+      Sedlexing.Latin1.from_string sp |> Parser.parse
+      |> Program.val_to_const !wsz
+      |> List.mapi ~f:(fun j i ->
+          match i with PUSH (Const _) -> PUSH (Const (Int.to_string j)) | _ -> i)
+
+    let equal sp1 sp2 =
+      let p1 = abstract_pusharg sp1
+      and p2 = abstract_pusharg sp2 in
+      Program.equal p1 p2
+
+    let compare sp1 sp2 =
+      if equal sp1 sp2 then 0
+      else
+        let lc = Int.compare (String.length sp1) (String.length sp2) in
+        if lc <> 0 then lc else String.compare sp1 sp2
+  end
+
+  include T
+  include Comparable.Make(T)
+end
 
 let () =
   let open Command.Let_syntax in
@@ -25,11 +37,22 @@ let () =
       let f = anon ("CSVFILE" %: string)
       and wordsize = flag "word-size" (required int)
           ~doc:"wsz word size, i.e., number of bits used for stack elements"
+      and outfile = flag "outfile" (optional_with_default "sorted.csv" string)
+          ~doc:"f.csv save result to f.csv"
       in
       fun () ->
+        Snippet_mod.set_wsz wordsize;
+        let m = Map.empty (module Snippet_mod) in
         let c = Csv.Rows.load ~has_header:true f |> List.map ~f:Csv.Row.to_list in
         let ps = List.map c ~f:List.hd_exn in
-        let ps = List.dedup_and_sort ~compare:(compare_prog wordsize) ps in
-        List.iter ps ~f:(print_endline)
+        let m =
+          List.fold_left ps ~init:m
+            ~f:(Map.update ~f:(function | None -> Z.one | Some n -> Z.succ n))
+        in
+        let outcsv =
+          List.map (Map.to_alist ~key_order:`Increasing m)
+            ~f:(fun (k, d) -> [k; Z.to_string d])
+        in
+        Csv.save outfile (["byte code"; "count"] :: outcsv)
     ]
   |> Command.run
