4838a0c88847d0aee9014ada85570f867ee71a5f
Julian Nagele <mail@jnagele.net>
2018-12-16 16:14:52 +0000

    Cleanup

--- examples/01-zero-/zero-.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.0;
-
-contract Zero {
-
-function f (uint8 x) public pure returns (uint8) {
-  return 42 + (0 - x);
-}
-
-}
--- examples/04-shift/shift.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.0;
-
-contract Shift {
-
-  function f (uint8 x) public returns (uint8) {
-    return x << 1;
-  }
-
-}
