e933d3cc8385395ff5d75f27fb450bfd26712d55
Julian Nagele <mail@jnagele.net>
2019-03-16 17:52:19 +0000

    Compute word size from program if not given on command line

--- ebso.ml
+++ ebso.ml
@@ -30,8 +30,8 @@ let outputcfg =
 
 let set_options wordsize stackas pm psmt pc pinter csv =
   outputcfg := {pmodel = pm; psmt = psmt; pcnstrnt = pc; pinter = pinter; csv = csv};
-  Option.iter wordsize ~f:(fun wordsize -> set_wsz wordsize);
-  Option.iter stackas ~f:(fun stackas -> set_sas stackas)
+  Option.iter stackas ~f:(fun stackas -> set_sas stackas);
+  set_wsz wordsize
 
 let log e =
   let log b s = if b then Out_channel.prerr_endline s else () in
@@ -214,13 +214,14 @@ let () =
       and progr = anon ("PROGRAM" %: string)
       in
       fun () ->
-        set_options wordsize stackas p_model p_smt p_constr p_inter csv;
         let buf =
           if direct then Sedlexing.Latin1.from_string progr
           else Sedlexing.Latin1.from_channel (In_channel.create progr)
         in
         let p = Parser.parse buf in
-        let p = if Option.is_some wordsize then Program.val_to_const !wsz p else p in
+        let wordsize = Option.value wordsize ~default:(Program.compute_word_size p 256) in
+        set_options wordsize stackas p_model p_smt p_constr p_inter csv;
+        let p = Program.val_to_const !wsz p in
         let bbs = Program.split_into_bbs p in
         match opt_mode with
         | NO ->
