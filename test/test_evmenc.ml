9b749daf3313895d2d0b930200db0191e0d034c2
Julian Nagele <mail@jnagele.net>
2019-03-22 17:28:24 +0000

    Rename variable xsload to xsstore to reflect its origin

--- test/test_evmenc.ml
+++ test/test_evmenc.ml
@@ -1414,10 +1414,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           5006
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of a single SSTORE with value 0, with refund" >:: (fun _ ->
@@ -1427,10 +1427,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           (-9994)
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of a single SSTORE with non-zero value, overwriting zero" >:: (fun _ ->
@@ -1440,10 +1440,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           20006
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of a single SSTORE with non-zero value, overwriting non-zero" >:: (fun _ ->
@@ -1453,10 +1453,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           5006
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing 0 to same key twice, no refund" >:: (fun _ ->
@@ -1466,10 +1466,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           10012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing 0 to same key twice, with refund" >:: (fun _ ->
@@ -1479,10 +1479,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           (-4988)
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing 0 then non-zero to same key, no refund" >:: (fun _ ->
@@ -1492,10 +1492,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           25012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing 0 then non-zero to same key, with refund" >:: (fun _ ->
@@ -1505,10 +1505,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           10012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing non-zero then zero to same key, overwriting zero" >:: (fun _ ->
@@ -1518,10 +1518,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           10012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing non-zero then zero to same key, overwriting non-zero" >:: (fun _ ->
@@ -1531,10 +1531,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           (-4988)
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing non-zero to same key twice, overwriting zero" >:: (fun _ ->
@@ -1544,10 +1544,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 0 in
+        let xsstore0 = senum 0 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           25012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
 
     "cost of SSTORing non-zero to same key twice, overwriting non-zero" >:: (fun _ ->
@@ -1557,10 +1557,10 @@ let gas_cost =
         let st = mk_state ea "" in
         let c = foralls (forall_vars ea) (enc_program ea st) in
         let m = solve_model_exn [c] in
-        let xsload0 = senum 2 in
+        let xsstore0 = senum 2 in
         assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
           10012
-          (eval_gas ~xs:[xsload0] st m (List.length p))
+          (eval_gas ~xs:[xsstore0] st m (List.length p))
       );
   ]
 
