9947968fd43c27967b58ea4137d56c6205e75110
Julian Nagele <mail@jnagele.net>
2019-03-22 16:39:19 +0000

    Add tests for gas cost of SSTORE
    fail, beause no variables are generated for SSTORE

--- test/test_evmenc.ml
+++ test/test_evmenc.ml
@@ -1406,6 +1406,162 @@ let gas_cost =
           (total_gas_cost p)
           (eval_gas st m (List.length p))
       );
+
+    "cost of a single SSTORE with value 0, no refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          5006
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of a single SSTORE with value 0, with refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          (-9994)
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of a single SSTORE with non-zero value, overwriting zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "2"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          20006
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of a single SSTORE with non-zero value, overwriting non-zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "3"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          5006
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing 0 to same key twice, no refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE; PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          10012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing 0 to same key twice, with refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE; PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          (-4988)
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing 0 then non-zero to same key, no refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE; PUSH (Val "2"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          25012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing 0 then non-zero to same key, with refund" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "0"); PUSH k; SSTORE; PUSH (Val "2"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          10012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing non-zero then zero to same key, overwriting zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "2"); PUSH k; SSTORE; PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          10012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing non-zero then zero to same key, overwriting non-zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "2"); PUSH k; SSTORE; PUSH (Val "0"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          (-4988)
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing non-zero to same key twice, overwriting zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "2"); PUSH k; SSTORE; PUSH (Val "3"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 0 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          25012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
+
+    "cost of SSTORing non-zero to same key twice, overwriting non-zero" >:: (fun _ ->
+        let k = Stackarg.Val "1" in
+        let p = [PUSH (Val "2"); PUSH k; SSTORE; PUSH (Val "3"); PUSH k; SSTORE] in
+        let ea = mk_enc_consts p (`User []) in
+        let st = mk_state ea "" in
+        let c = enc_program ea st in
+        let m = solve_model_exn [c] in
+        let xsload0 = senum 2 in
+        assert_equal ~cmp:[%eq: int] ~printer:Int.to_string
+          10012
+          (eval_gas ~xs:[xsload0] st m (List.length p))
+      );
   ]
 
 let storage =
