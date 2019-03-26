12203fef16d25e893b64da927a139c7fa071b263
Maria A Schett <mail@maria-a-schett.net>
2019-03-26 15:03:21 +0000

    Inline uso_incr_wsz

--- ebso.ml
+++ ebso.ml
@@ -89,12 +89,10 @@ let rec uso p hist cis tval hist_bbs =
   then hist :: hist_bbs
   else
     match stp.tval with
-    | Some false -> uso_incr_wsz p hist cis tval hist_bbs
+    | Some false ->
+      set_wsz (!wsz + 1);
+      uso (Program.val_to_const !wsz (Program.const_to_val p)) hist cis tval hist_bbs
     | _ -> uso (stp.opt) hist cis tval hist_bbs
-and
-  uso_incr_wsz p hist cis tval hist_bbs =
-    set_wsz (!wsz + 1);
-    uso (Program.val_to_const !wsz (Program.const_to_val p)) hist cis tval hist_bbs
 
 let super_optimize_encbl p cis tval hist_bbs = uso p [] cis tval hist_bbs
 
